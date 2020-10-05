337258d6191d309aefa7cdc284af8f75cc5d86c2
Rafal Lesnicki <lesnickirafal@gmail.com>
2020-10-05 10:12:55 +0200

    Rozwiązanie zadania nformacje o produktach - zmienne

---
+++ src/lekcja2_zadania/Informacje_o_produktach_zmienne/Product.java
@@ -0,0 +1,7 @@
+package lekcja2_zadania.Informacje_o_produktach_zmienne;
+
+public class Product {
+    String productName;
+    String producer;
+    double price;
+}
---
+++ src/lekcja2_zadania/Informacje_o_produktach_zmienne/Shop.java
@@ -0,0 +1,20 @@
+package lekcja2_zadania.Informacje_o_produktach_zmienne;
+
+public class Shop {
+    public static void main(String[] args) {
+
+        Product firstProduct = new Product();
+        firstProduct.productName = "Mleko";
+        firstProduct.producer = "Mlekowita";
+        firstProduct.price = 2.5;
+
+        Product secondProduct = new Product();
+        secondProduct.productName = "Czekolada";
+        secondProduct.producer = "Wedel";
+        secondProduct.price = 2.19;
+
+        System.out.println(firstProduct.productName + " " + firstProduct.producer + " " + firstProduct.price + "zł");
+        System.out.println(secondProduct.productName + " " + secondProduct.producer + " " + secondProduct.price + "zł");
+
+    }
+}
