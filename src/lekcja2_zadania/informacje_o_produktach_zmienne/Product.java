a50bb1da431ebfde42f94094adb96735f7c7a6ae
Rafal Lesnicki <lesnickirafal@gmail.com>
2020-10-05 18:47:25 +0200

    Fix shop and product.

--- src/lekcja2_zadania/informacje_o_produktach_zmienne/Product.java
+++ src/lekcja2_zadania/informacje_o_produktach_zmienne/Product.java
@@ -1,10 +1,7 @@
 package lekcja2_zadania.informacje_o_produktach_zmienne;
 
 public class Product {
-    String firstProductName;
-    String firstProductProducer;
-    double firstProductPrice;
-    String secondProductName;
-    String secondProductProducer;
-    double secondProductPrice;
+    String productName;
+    String producer;
+    double price;
 }
--- src/lekcja2_zadania/informacje_o_produktach_zmienne/Shop.java
+++ src/lekcja2_zadania/informacje_o_produktach_zmienne/Shop.java
@@ -3,18 +3,17 @@ package lekcja2_zadania.informacje_o_produktach_zmienne;
 public class Shop {
     public static void main(String[] args) {
 
-        Product product = new Product();
-        product.firstProductName = "Mleko";
-        product.firstProductProducer = "Mlekowita";
-        product.firstProductPrice = 2.5;
+        Product firstProduct = new Product();
+        firstProduct.productName = "Mleko";
+        firstProduct.producer = "Mlekowita";
+        firstProduct.price = 2.5;
 
-        product.secondProductName = "Czekolada";
-        product.secondProductProducer = "Wedel";
-        product.secondProductPrice = 2.19;
+        Product secondProduct = new Product();
+        secondProduct.productName = "Czekolada";
+        secondProduct.producer = "Wedel";
+        secondProduct.price = 2.19;
 
-        System.out.println(product.firstProductName + " " + product.firstProductProducer + " "
-                + product.firstProductPrice + "zł");
-        System.out.println(product.secondProductName + " " + product.secondProductProducer + " "
-                + product.secondProductPrice + "zł");
+        System.out.println(firstProduct.productName + " " + firstProduct.producer + " " + firstProduct.price + "zł");
+        System.out.println(secondProduct.productName + " " + secondProduct.producer + " " + secondProduct.price + "zł");
     }
 }
