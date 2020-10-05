367e8a97399a69aa57a8437170c3f03a8d44f4f8
Rafal Lesnicki <lesnickirafal@gmail.com>
2020-10-05 11:54:47 +0200

    Rozwiązanie zadania fix Bar and Drink

--- src/lekcja2_zadania/drink/Bar.java
+++ src/lekcja2_zadania/drink/Bar.java
@@ -6,26 +6,30 @@ public class Bar {
         drink1.name = "Woo Woo";
         drink1.price = 30;
         drink1.includesAlcohol = true;
-        drink1.ingredients1.ingredientsName = "Peach schnapps";
-        drink1.ingredients1.ingredientsQuantity = 0.025;
-        drink1.ingredients2.ingredientsName = "Vodka";
-        drink1.ingredients2.ingredientsQuantity = 0.025;
-        drink1.ingredients3.ingredientsName = "Cranberry juice";
-        drink1.ingredients3.ingredientsQuantity = 0.050;
+
+        drink1.firstIngredients = new Ingredients();
+        drink1.firstIngredients.ingredientsName = "Peach schnapps";
+        drink1.firstIngredients.ingredientsQuantity = 0.025;
+        drink1.secondIngredients = new Ingredients();
+        drink1.secondIngredients.ingredientsName = "Vodka";
+        drink1.secondIngredients.ingredientsQuantity = 0.025;
+        drink1.thirdIngredients = new Ingredients();
+        drink1.thirdIngredients.ingredientsName = "Cranberry juice";
+        drink1.thirdIngredients.ingredientsQuantity = 0.050;
 
         System.out.println("Drink: " + drink1.name + ",price: " + drink1.price + "zł" + ", includes alcohol: "
                 + drink1.includesAlcohol);
         System.out.println("Ingredients:");
-        System.out.println("First ingredient: " + drink1.ingredients1.ingredientsName + ", quantity: "
-                + drink1.ingredients1.ingredientsQuantity + "L");
-        System.out.println("Second ingredient: " + drink1.ingredients2.ingredientsName + ", quantity: "
-                + drink1.ingredients2.ingredientsQuantity + "L");
-        System.out.println("Third ingredient: " + drink1.ingredients3.ingredientsName + ", quantity: "
-                + drink1.ingredients3.ingredientsQuantity + "L");
+        System.out.println("First ingredient: " + drink1.firstIngredients.ingredientsName + ", quantity: "
+                + drink1.firstIngredients.ingredientsQuantity + "L");
+        System.out.println("Second ingredient: " + drink1.secondIngredients.ingredientsName + ", quantity: "
+                + drink1.secondIngredients.ingredientsQuantity + "L");
+        System.out.println("Third ingredient: " + drink1.thirdIngredients.ingredientsName + ", quantity: "
+                + drink1.thirdIngredients.ingredientsQuantity + "L");
 
-        double drinkCapacity = drink1.ingredients1.ingredientsQuantity + drink1.ingredients2.ingredientsQuantity
-                + drink1.ingredients3.ingredientsQuantity;
+        double drinkCapacity = drink1.firstIngredients.ingredientsQuantity + drink1.secondIngredients.ingredientsQuantity
+                + drink1.thirdIngredients.ingredientsQuantity;
 
-        System.out.println("Drink capacity: " + drink1.ingredients1.ingredientsName + " = " + drinkCapacity + "L");
+        System.out.println("Drink capacity: " + drink1.firstIngredients.ingredientsName + " = " + drinkCapacity + "L");
     }
 }
