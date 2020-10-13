0cfaeffa4a3f27f89373f1fb8f3e57f2cc3e7e25
Rafal Lesnicki <lesnickirafal@gmail.com>
2020-10-13 08:44:54 +0200

    Fix, dodanie specyfikatora dostępu public i zmiana nazwy klasy na Task

--- src/lekcja4_zadanie3_todo/ToDoListTest.java
+++ src/lekcja4_zadanie3_todo/ToDoListTest.java
@@ -4,11 +4,11 @@ public class ToDoListTest {
     public static void main(String[] args) {
         Person person1 = new Person("Rafal", "Kowalski");
         Person person2 = new Person("Paweł", "Kowalkiewicz");
-        ToDoList toDoList1 = new ToDoList("Sprzątanie", "Sprzątanie salonu.", 0, person1);
-        ToDoList toDoList2 = new ToDoList("Zakupy", "Kupienie artykułów na obiad.", 1, person2);
-        ToDoList toDoList3 = new ToDoList("Zmywanie", "Zmywanie naczyń.", -1, person1);
-        ToDoList toDoList4 = new ToDoList("Podlewanie", "Podlewanie kwiatów.", 0);
-        ToDoList toDoList5 = new ToDoList("Sprzątanie", "Sprzątanie balkonu.");
+        Task toDoList1 = new Task("Sprzątanie", "Sprzątanie salonu.", 0, person1);
+        Task toDoList2 = new Task("Zakupy", "Kupienie artykułów na obiad.", 1, person2);
+        Task toDoList3 = new Task("Zmywanie", "Zmywanie naczyń.", -1, person1);
+        Task toDoList4 = new Task("Podlewanie", "Podlewanie kwiatów.", 0);
+        Task toDoList5 = new Task("Sprzątanie", "Sprzątanie balkonu.");
 
         toDoList1.showAllInfo();
         System.out.println();
