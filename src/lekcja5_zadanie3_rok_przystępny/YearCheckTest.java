b18da5f559b07c82c5a047bcf758ac5924e25f25
Rafal Lesnicki <lesnickirafal@gmail.com>
2020-10-15 21:09:16 +0200

    Lekcja 5, zadanie 3 Rok przestępny

---
+++ src/lekcja5_zadanie3_rok_przystępny/YearCheckTest.java
@@ -0,0 +1,8 @@
+package lekcja5_zadanie3_rok_przystępny;
+
+public class YearCheckTest {
+    public static void main(String[] args) {
+        YearCheck leapYear = new YearCheck();
+        System.out.println("Rok przestępny: " + leapYear.isLeap(2012));
+    }
+}
