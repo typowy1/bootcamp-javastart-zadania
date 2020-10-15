f18942722a9dc5e721b38049b350f8858c687845
Rafal Lesnicki <lesnickirafal@gmail.com>
2020-10-15 21:22:14 +0200

    Lekcja 5, zadanie 3 Rok przestępny, fix

--- src/lekcja5_zadanie3_rok_przestępny/YearCheck.java
+++ src/lekcja5_zadanie3_rok_przestępny/YearCheck.java
@@ -2,10 +2,6 @@ package lekcja5_zadanie3_rok_przestępny;
 
 public class YearCheck {
     boolean isLeap(int year) {
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
     }
 }
--- src/lekcja5_zadanie3_rok_przestępny/YearCheckTest.java
+++ src/lekcja5_zadanie3_rok_przestępny/YearCheckTest.java
@@ -3,6 +3,6 @@ package lekcja5_zadanie3_rok_przestępny;
 public class YearCheckTest {
     public static void main(String[] args) {
         YearCheck leapYear = new YearCheck();
-        System.out.println("Rok przestępny: " + leapYear.isLeap(2100));
+        System.out.println("Rok przestępny: " + leapYear.isLeap(2020));
     }
 }
