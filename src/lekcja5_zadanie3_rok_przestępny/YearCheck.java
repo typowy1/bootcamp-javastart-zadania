db6996c9221a210fd632ff92e5225ff148280e08
Rafal Lesnicki <lesnickirafal@gmail.com>
2020-10-15 21:17:01 +0200

    Lekcja 5, zadanie 3 Rok przestępny

---
+++ src/lekcja5_zadanie3_rok_przestępny/YearCheck.java
@@ -0,0 +1,11 @@
+package lekcja5_zadanie3_rok_przestępny;
+
+public class YearCheck {
+    boolean isLeap(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ src/lekcja5_zadanie3_rok_przestępny/YearCheckTest.java
@@ -0,0 +1,8 @@
+package lekcja5_zadanie3_rok_przestępny;
+
+public class YearCheckTest {
+    public static void main(String[] args) {
+        YearCheck leapYear = new YearCheck();
+        System.out.println("Rok przestępny: " + leapYear.isLeap(2100));
+    }
+}
