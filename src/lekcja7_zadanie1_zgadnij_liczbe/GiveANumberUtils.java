715208a545c50bcef46cb8359c72f56a0780d719
Rafal Lesnicki <lesnickirafal@gmail.com>
2020-10-23 10:09:43 +0200

    Fix

--- src/lekcja7_zadanie1_zgadnij_liczbe/GiveANumberUtils.java
+++ src/lekcja7_zadanie1_zgadnij_liczbe/GiveANumberUtils.java
@@ -6,19 +6,19 @@ public class GiveANumberUtils {
 
     public void findNumber(int from, int to, int divisibleBy) {
         Scanner scanner = new Scanner(System.in);
-        int numberInput;
+        int inputNumber;
 
         do {
             System.out.println("Podaj liczbę");
-            numberInput = scanner.nextInt();
-            if (numberInput < from) {
+            inputNumber = scanner.nextInt();
+            if (inputNumber < from) {
                 System.out.println("Podana Liczba jest za mała");
-            } else if (numberInput > to) {
+            } else if (inputNumber > to) {
                 System.out.println("Podana liczba jest za duża");
-            } else if (numberInput % divisibleBy != 0) {
+            } else if (inputNumber % divisibleBy != 0) {
                 System.out.println("Liczba nie jest podzielna przez 3");
             }
-        } while (numberInput < from || numberInput > to || numberInput % divisibleBy != 0);
+        } while (inputNumber < from || inputNumber > to || inputNumber % divisibleBy != 0);
         System.out.println("Liczba jest ok");
         scanner.close();
     }
