65f04f3babb59581aaf1e36c5efafd473bcabb15
Rafal Lesnicki <lesnickirafal@gmail.com>
2020-10-23 08:49:33 +0200

    Lekcja 7, zadanie 1, zgadnij liczbe

---
+++ src/lekcja7_zadanie1_zgadnij_liczbe/GiveANumberUtils.java
@@ -0,0 +1,25 @@
+package lekcja7_zadanie1_zgadnij_liczbe;
+
+import java.util.Scanner;
+
+public class GiveANumberUtils {
+
+    public void findNumber(int from, int to, int divisibleBy) {
+        Scanner scanner = new Scanner(System.in);
+        double numberInput;
+
+        do {
+            System.out.println("Podaj liczbę");
+            numberInput = scanner.nextInt();
+            if (numberInput < from) {
+                System.out.println("Podana Liczba jest za mała");
+            } else if (numberInput > to) {
+                System.out.println("Podana liczba jest za duża");
+            } else if (numberInput % divisibleBy != 0) {
+                System.out.println("Liczba nie jest podzielna przez 3");
+            }
+        } while (numberInput < from || numberInput > to || numberInput % divisibleBy != 0);
+        System.out.println("Liczba jest ok");
+        scanner.close();
+    }
+}
