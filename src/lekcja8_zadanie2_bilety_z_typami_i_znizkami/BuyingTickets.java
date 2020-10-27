61b9d2db69bd74d2bd29b977fc2ef38c0378cfc4
Rafal Lesnicki <lesnickirafal@gmail.com>
2020-10-27 22:02:57 +0100

    fix

--- src/lekcja8_zadanie2_bilety_z_typami_i_znizkami/BuyingTickets.java
+++ src/lekcja8_zadanie2_bilety_z_typami_i_znizkami/BuyingTickets.java
@@ -3,7 +3,6 @@ package lekcja8_zadanie2_bilety_z_typami_i_znizkami;
 public class BuyingTickets {
     public static void main(String[] args) {
 
-
         Ticket[] tickets = TicketUtils.enterTickets();
         TicketUtils.showInfo(tickets);
     }
