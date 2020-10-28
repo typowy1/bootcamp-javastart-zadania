936f54e4ee03b91e7a8b392fe806773ac4960ee2
Rafal Lesnicki <lesnickirafal@gmail.com>
2020-10-28 18:15:19 +0100

    Lekcja8, zadanie1, część 2, added addGrade method

--- src/lekcja8_zadanie1_firma_szkoleniowa/Student.java
+++ src/lekcja8_zadanie1_firma_szkoleniowa/Student.java
@@ -2,7 +2,9 @@ package lekcja8_zadanie1_firma_szkoleniowa;
 
 public class Student extends Person {
 
-    private Grade[] grades;
+    public static final int MAX_GRADE_SIZE = 10;
+    private Grade[] grades = new Grade[MAX_GRADE_SIZE];
+    private int currentGradeAmount = 0;
 
     public Student(String firstName, String lastName, Grade[] grades) {
         super(firstName, lastName);
@@ -20,5 +22,10 @@ public class Student extends Person {
 
     public void setGrades(Grade[] grades) {
         this.grades = grades;
+    }
+
+    public void addGrade(Grade grade){
+        grades[currentGradeAmount] = grade;
+        currentGradeAmount++;
     }
 }
