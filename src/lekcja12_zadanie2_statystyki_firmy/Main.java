f041a0b11613b8a90ec1c7df88b7fec66ca9cc0d
Rafal Lesnicki <lesnickirafal@gmail.com>
2020-11-14 11:12:18 +0100

    rozwiązania lekcja 12 fix

--- src/lekcja12_zadanie2_statystyki_firmy/Main.java
+++ src/lekcja12_zadanie2_statystyki_firmy/Main.java
@@ -3,14 +3,19 @@ package lekcja12_zadanie2_statystyki_firmy;
 import java.io.File;
 import java.io.IOException;
 
+import static lekcja12_zadanie2_statystyki_firmy.EmployeeUtils.*;
+import static lekcja12_zadanie2_statystyki_firmy.EmployeeUtils.save1;
+
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        File file = new File("employees.csv");
-        Employee[] employees = EmployeeUtils.read(file);
+        //musiałem dac statyczne importy bo jakies błędy wyskoczyły po skopiowaniu klasy z repo
 
-        EmployeeUtils.printStatisticsInfo(employees);
-        EmployeeUtils.save(employees, "stats.txt");
+        File file = new File("employees.csv");
+        Employee[] employees = read1(file);
+
+        printStatisticsInfo(employees);
+        save1(employees, "stats.txt");
     }
 }
